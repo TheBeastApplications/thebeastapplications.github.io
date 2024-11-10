e8e1b6fdea7c9e52422a039c135ce4f63bab7814
Shuvam Jaswal <74697475+ShuvamJaswal@users.noreply.github.com>
2024-11-10 18:13:59 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: "/thebeastapplications.github.io/",
   server: {
     host: true,
     port: '8080',
