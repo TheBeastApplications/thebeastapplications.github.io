83613ba5e360ebfba69370f4f4f50de8dfdc8b59
Shuvam Jaswal <74697475+ShuvamJaswal@users.noreply.github.com>
2024-11-11 18:41:29 +0530

    Update routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import BalanceBookRebornHome from "./Pages/apps/balance_book_reborn/balanceBookRebornHome";
 import BalanceBookPrivacyPolicy from "./Pages/apps/balance_book_reborn/balanceBookPrivacyPolicy";
 import BalanceBookDeleteAccount from "./Pages/apps/balance_book_reborn/balanceBookDeleteAccount";
@@ -7,7 +7,7 @@ import HomePage from "./Pages/HomePage/HomePage";
 function App() {
   return (
     <>
-   <BrowserRouter>
+   <HashRouter>
   <Routes>
     <Route index element={<HomePage />} />
     <Route path="/app/balance_book_reborn">
@@ -16,7 +16,7 @@ function App() {
       <Route path="delete-account" element={<BalanceBookDeleteAccount />} />
     </Route>
   </Routes>
-</BrowserRouter>
+</HashRouter>
     </>
   );
 }
